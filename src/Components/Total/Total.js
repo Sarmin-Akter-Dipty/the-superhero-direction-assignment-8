63861aa942ff234a954f2304d75f5cde56dca909
Kaushik-Pramanik <pramanikk33@gmail.com>
2021-09-26 23:47:26 +0600

    some correction

--- src/Components/Total/Total.js
+++ src/Components/Total/Total.js
@@ -7,14 +7,14 @@ const Total = (props) => {
     let Name = []
     for (const fish of addedFish) {
         Total = Total + fish.Price
-        Name = Name + fish.Name
+        Name = Name + fish.Name + ", "
     }
 
     return (
         <div>
             <h3 className="color">Total Price:{Total}</h3>
-            <h3 className="color">Name:{Name}</h3>
-        </div>
+            <h3 className="color"> Name:{Name} </h3>
+        </div >
     );
 
 };
